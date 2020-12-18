aed2cc377b25d3a9f04320937d40265762d47518
Jessica <duellje@oregonstate.edu>
2020-12-18 17:43:22 -0500

    added myers reset and general styles

--- movies/src/components/SearchBar.js
+++ movies/src/components/SearchBar.js
@@ -1,7 +1,20 @@
+//styles
+import "../styles/SearchBar.scss";
+
+//images
+import magnifying from "../imgs/magnifying_glass.png";
+
 const SearchBar = () => {
   return (
-    <div>
-      <h1>Movie Title</h1>
+    <div className="searchBar">
+      <form>
+        <label htmlFor="movie">Movie Title</label>
+        <div className="box">
+          <img src={magnifying} alt="Search" className="glass" />
+          <input type="text" id="movie" />
+        </div>
+        <button>Search</button>
+      </form>
     </div>
   );
 };
